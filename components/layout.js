3e4dfdde3e657eb5ba6da3f062752f0adac5d551
kunpriyaa <kunpriyachaiwong21@!gmail.com>
2024-11-24 22:51:33 +0700

    v2

--- components/layout.js
+++ components/layout.js
@@ -6,7 +6,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = '[kunpriyaa]';
+const name = '[kunpriyaa-]';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
