746f920e2bd36a32e02f947719a49989e511fb66
kunpriyaa <kunpriyachaiwong21@!gmail.com>
2024-11-30 00:27:38 +0700

    figma

--- components/layout.js
+++ components/layout.js
@@ -12,7 +12,6 @@ export const siteTitle = 'Next.js Sample Website';
 export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
-      {/* Meta Tags */}
       <Head>
         <link rel="icon" href="/favicon.ico" />
         <meta
@@ -29,7 +28,6 @@ export default function Layout({ children, home }) {
         <meta name="twitter:card" content="summary_large_image" />
       </Head>
 
-      {/* Script Loading */}
       <Script
         src="https://connect.facebook.net/en_US/sdk.js"
         strategy="lazyOnload"
@@ -38,7 +36,6 @@ export default function Layout({ children, home }) {
         }
       />
 
-      {/* Header Section */}
       <header className={styles.header}>
         {home ? (
           <>
@@ -73,34 +70,71 @@ export default function Layout({ children, home }) {
         )}
       </header>
 
-      {/* Main Content */}
       <main className={styles.mainContent}>
         {children}
-        {/* Yourself Introduction Section */}
-        <section className={styles.personalInfo}>
-          <h3>Yourself Introduction</h3>
-          <p>
-            <strong>Student ID:</strong> 66024974<br />
-            <strong>Name:</strong> Miss Kunpriyaa Chaiyavong<br />
-            <strong>Education:</strong> Currently studying at <i>University of Phayao</i>, 
-            Faculty of <strong>Information and Communication Technology</strong>, 
-            Major in Software Engineering, 2nd Year<br />
-            <strong>GitHub:</strong> <a href="https://github.com/kunpriyaa" target="_blank" rel="noopener noreferrer">https://github.com/kunpriyaa</a><br />
-            <strong>Vercel:</strong> <a href="https://vercel.com/kunpriyaas-projects" target="_blank" rel="noopener noreferrer">https://vercel.com/kunpriyaas-projects</a>
-          </p>
 
-          <h3>Skills</h3>
-          <ul>
-            <li>JavaScript, TypeScript</li>
-            <li>React, Next.js</li>
-            <li>Node.js, Express</li>
-            <li>Database: MySQL, MongoDB</li>
-            <li>Version Control: Git, GitHub</li>
-          </ul>
+        {/* Your Figma Works */}
+        <section className={styles.figmaWorks}>
+          <h3>My Figma Designs</h3>
+          
+          <div className={styles.figmaEmbed}>
+            <h4>Project 1</h4>
+            <iframe 
+              style="border: 1px solid rgba(0, 0, 0, 0.1);" 
+              width="800" 
+              height="450" 
+              src="https://embed.figma.com/proto/6ikLfb0JnMkyE5yJAG1fbu/%E0%B8%A3%E0%B8%A7%E0%B8%A1%E0%B8%A3%E0%B8%A7%E0%B8%9A%E0%B8%82%E0%B9%89%E0%B8%AD%E0%B8%A1%E0%B8%B9%E0%B8%A5%E0%B8%81%E0%B8%B2%E0%B9%81%E0%B8%9F?node-id=3-1049&node-type=canvas&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=3%3A1049&share=1&embed-host=share" 
+              allowfullscreen>
+            </iframe>
+          </div>
+
+          <div className={styles.figmaEmbed}>
+            <h4>Project 2</h4>
+            <iframe 
+              style="border: 1px solid rgba(0, 0, 0, 0.1);" 
+              width="800" 
+              height="450" 
+              src="https://embed.figma.com/design/LSqOwVcMMqb9maLG0xSorA/Group-7-%3A-Selling-Tree?node-id=0-1&embed-host=share" 
+              allowfullscreen>
+            </iframe>
+          </div>
+
+          <div className={styles.figmaEmbed}>
+            <h4>Project 3</h4>
+            <iframe 
+              style="border: 1px solid rgba(0, 0, 0, 0.1);" 
+              width="800" 
+              height="450" 
+              src="https://embed.figma.com/design/1u3qFyZeuFzwXUXSiyccjg/Plant-App-the-sill?node-id=0-1&embed-host=share" 
+              allowfullscreen>
+            </iframe>
+          </div>
+
+          <div className={styles.figmaEmbed}>
+            <h4>Project 4</h4>
+            <iframe 
+              style="border: 1px solid rgba(0, 0, 0, 0.1);" 
+              width="800" 
+              height="450" 
+              src="https://embed.figma.com/design/5qg65RFgJymyTxKycIH5vf/%E0%B9%81%E0%B8%AD%E0%B8%9B%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B8%A0%E0%B8%B2%E0%B8%9E%E0%B8%88%E0%B8%B4%E0%B8%951?node-id=0-1&embed-host=share" 
+              allowfullscreen>
+            </iframe>
+          </div>
+
+          <div className={styles.figmaEmbed}>
+            <h4>Project 5</h4>
+            <iframe 
+              style="border: 1px solid rgba(0, 0, 0, 0.1);" 
+              width="800" 
+              height="450" 
+              src="https://embed.figma.com/design/ai0Zr528lI8RsLZJcoYVrm/Plant-App-the-sill-66024974?embed-host=share" 
+              allowfullscreen>
+            </iframe>
+          </div>
         </section>
+
       </main>
 
-      {/* Footer */}
       {!home && (
         <div className={styles.backToHome}>
           <Link href="/">← Back to home</Link>
