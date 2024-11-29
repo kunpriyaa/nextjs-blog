27ad667e79f7654fc64717c1eb25a57d1875047e
kunpriyaa <kunpriyachaiwong21@!gmail.com>
2024-11-30 01:01:30 +0700

    4

--- components/layout.js
+++ components/layout.js
@@ -1,7 +1,6 @@
 import Head from 'next/head';
 import Image from 'next/image';
 import Script from 'next/script';
-
 import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
@@ -82,9 +81,8 @@ export default function Layout({ children, home }) {
           <h3>Yourself Introduction</h3>
           <p>
             <strong>Student ID:</strong> 66024974<br />
-            <strong>Name:</strong> Miss Kunpriyaa Chaiwong<br />
-            <strong>Education:</strong> Currently studying at <i>University of Phayao</i>, 
-            Faculty of Information and Communication Technology (ICT), Software Engineering, Year 2
+            <strong>Name:</strong> Ms. Kunpriyaa Chaiyawong<br />
+            <strong>Education:</strong> Currently studying at <i>University of Phayao</i>, Faculty of ICT, Software Engineering, 2nd Year
           </p>
         </section>
 
@@ -100,24 +98,33 @@ export default function Layout({ children, home }) {
           </ul>
         </section>
 
-        {/* Figma Project Showcase Section */}
+        {/* Figma Projects Section */}
         <section className={styles.projectsSection}>
-          <h3>Figma Projects</h3>
-
+          <h3>My Figma Projects</h3>
           <div className={styles.projectsGrid}>
-            {/* Figma Project 1 */}
+            {/* Project 1 */}
             <div className={styles.projectItem}>
-              <h4>Project 1: Figma Design</h4>
+              <h4>Project 1</h4>
               <iframe
                 className={styles.figmaFrame}
                 src="https://embed.figma.com/proto/6ikLfb0JnMkyE5yJAG1fbu/%E0%B8%A3%E0%B8%A7%E0%B8%A1%E0%B8%A3%E0%B8%A7%E0%B8%9A%E0%B8%82%E0%B9%89%E0%B8%AD%E0%B8%A1%E0%B8%B9%E0%B8%A5%E0%B8%81%E0%B8%B2%E0%B9%81%E0%B8%9F?node-id=3-1049&node-type=canvas&scaling=min-zoom&content-scaling=fixed&page-id=0%3A1&starting-point-node-id=3%3A1049&share=1&embed-host=share"
                 allowFullScreen
               ></iframe>
             </div>
-
-            {/* Figma Project 2 */}
+            
+            {/* Project 2 */}
             <div className={styles.projectItem}>
-              <h4>Project 2: Figma Design</h4>
+              <h4>Project 2</h4>
+              <iframe
+                className={styles.figmaFrame}
+                src="https://embed.figma.com/design/LSqOwVcMMqb9maLG0xSorA/Group-7-%3A-Selling-Tree?node-id=0-1&embed-host=share"
+                allowFullScreen
+              ></iframe>
+            </div>
+
+            {/* Project 3 */}
+            <div className={styles.projectItem}>
+              <h4>Project 3</h4>
               <iframe
                 className={styles.figmaFrame}
                 src="https://embed.figma.com/design/1u3qFyZeuFzwXUXSiyccjg/Plant-App-the-sill?node-id=0-1&embed-host=share"
@@ -125,22 +132,12 @@ export default function Layout({ children, home }) {
               ></iframe>
             </div>
 
-            {/* Figma Project 3 */}
+            {/* Project 4 */}
             <div className={styles.projectItem}>
-              <h4>Project 3: Figma Design</h4>
+              <h4>Project 4</h4>
               <iframe
                 className={styles.figmaFrame}
                 src="https://embed.figma.com/design/5qg65RFgJymyTxKycIH5vf/%E0%B9%81%E0%B8%AD%E0%B8%9B%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B8%A0%E0%B8%B2%E0%B8%9E%E0%B8%88%E0%B8%B4%E0%B8%951?node-id=0-1&embed-host=share"
-                allowFullScreen
-              ></iframe>
-            </div>
-
-            {/* Figma Project 4 */}
-            <div className={styles.projectItem}>
-              <h4>Project 4: Figma Design</h4>
-              <iframe
-                className={styles.figmaFrame}
-                src="https://embed.figma.com/design/ai0Zr528lI8RsLZJcoYVrm/Plant-App-the-sill-66024974?embed-host=share"
                 allowFullScreen
               ></iframe>
             </div>
