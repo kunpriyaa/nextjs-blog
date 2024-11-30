5ab54ca0391cf26fa1b35dfa6a53f8d289290608
kunpriyaa <kunpriyachaiwong21@!gmail.com>
2024-11-30 23:09:14 +0700

    imageprofle2

--- components/layout.js
+++ components/layout.js
@@ -44,7 +44,7 @@ export default function Layout({ children, home }) {
           <>
             <Image
               priority
-              src="/images/profile.jpg"
+              src="public/images/Image.jpg"
               className={utilStyles.borderCircle}
               height={144}
               width={144}
