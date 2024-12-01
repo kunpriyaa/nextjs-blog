6c32d8f4dcbb09797d2d7dc03d3b80b97aeb8251
kunpriyaa <kunpriyachaiwong21@!gmail.com>
2024-12-01 21:34:41 +0700

    013

--- components/layout.js
+++ components/layout.js
@@ -111,45 +111,45 @@ export default function Layout({ children, home }) {
             <h3>My Figma Projects</h3>
             <div className={styles.projectsGrid}>
               <div className={styles.projectCard}>
-                <h4>Personal Portfolio</h4>
+                <h4>Online Coffee Shop</h4>
                 <iframe
-                  src="https://embed.figma.com/design/ai0Zr528lI8RsLZJcoYVrm/Personal-Portfolio"
-                  width="100%"
-                  height="400"
-                  frameBorder="0"
+                  style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
+                  width="800"
+                  height="450"
+                  src="https://embed.figma.com/design/6ikLfb0JnMkyE5yJAG1fbu/%E0%B8%A3%E0%B8%A7%E0%B8%A1%E0%B8%A3%E0%B8%A7%E0%B8%9A%E0%B8%82%E0%B9%89%E0%B8%AD%E0%B8%A1%E0%B8%B9%E0%B8%A5%E0%B8%81%E0%B8%B2%E0%B9%81%E0%B8%9F?node-id=0-1&embed-host=share"
                   allowFullScreen
-                  title="Personal Portfolio"
+                  title="Online Coffee Shop"
                 ></iframe>
               </div>
               <div className={styles.projectCard}>
-                <h4>Selling Tree App</h4>
+                <h4>Selling Tree Website</h4>
                 <iframe
-                  src="https://embed.figma.com/design/LSqOwVcMMqb9maLG0xSorA/Group-7-%3A-Selling-Tree"
-                  width="100%"
-                  height="400"
-                  frameBorder="0"
+                  style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
+                  width="800"
+                  height="450"
+                  src="https://embed.figma.com/design/LSqOwVcMMqb9maLG0xSorA/Group-7-%3A-Selling-Tree?node-id=0-1&embed-host=share"
                   allowFullScreen
-                  title="Selling Tree App"
+                  title="Selling Tree Website"
                 ></iframe>
               </div>
               <div className={styles.projectCard}>
-                <h4>Plant App Design</h4>
+                <h4>Plant App</h4>
                 <iframe
-                  src="https://embed.figma.com/design/1u3qFyZeuFzwXUXSiyccjg/Plant-App-the-sill"
-                  width="100%"
-                  height="400"
-                  frameBorder="0"
+                  style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
+                  width="800"
+                  height="450"
+                  src="https://embed.figma.com/design/ai0Zr528lI8RsLZJcoYVrm/Plant-App-the-sill-66024974?node-id=1810-144&embed-host=share"
                   allowFullScreen
-                  title="Plant App Design"
+                  title="Plant App"
                 ></iframe>
               </div>
               <div className={styles.projectCard}>
                 <h4>Health App UI</h4>
                 <iframe
-                  src="https://embed.figma.com/design/5qg65RFgJymyTxKycIH5vf/%E0%B9%81%E0%B8%AD%E0%B8%9B%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B8%A0%E0%B8%B2%E0%B8%9E%E0%B8%88%E0%B8%B4%E0%B8%951"
-                  width="100%"
-                  height="400"
-                  frameBorder="0"
+                  style={{ border: '1px solid rgba(0, 0, 0, 0.1)' }}
+                  width="800"
+                  height="450"
+                  src="https://embed.figma.com/design/9oYVg0DlOhtTAYryJyZxK8/%E0%B9%81%E0%B8%AD%E0%B8%9B%E0%B8%AA%E0%B8%B8%E0%B8%82%E0%B8%A0%E0%B8%B2%E0%B8%9E%E0%B8%88%E0%B8%B4%E0%B8%95?node-id=415-62&embed-host=share"
                   allowFullScreen
                   title="Health App UI"
                 ></iframe>
